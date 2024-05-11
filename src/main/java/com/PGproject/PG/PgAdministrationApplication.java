b799a900f8668b8c15ca0992f9c1e79363eee8ab
guptaG-21 <hg287431@gmail.com>
2024-05-11 14:25:43 +0530

    initial commit

---
+++ src/main/java/com/PGproject/PG/PgAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.PGproject.PG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgAdministrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PGproject/PG/PgAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PGproject.PG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
